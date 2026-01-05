fd0b555893f6c35bdf95e5cc3989d2c3771119e3
Armiya YOUSSOUF <armiya.youssouf@gmail.com>
2026-01-05 15:25:57 +0100

    Mon message de test avec print

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('Debut de l aventure')
+print("Debut de l'aventure")
+prenom = "Armia"
+Pays = "France"
+ville = "Paris"
+
+print(f" Bonjour, mon Prenom : {prenom}, mon Pays : {Pays} et ma Ville: {ville}")
