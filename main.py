38cde4b4dd5fb062fdc8f41c1730e69a4bec26a3
Armiya YOUSSOUF <armiya.youssouf@gmail.com>
2026-02-22 08:45:33 +0100

    Environnement pret : imports et infos personnelles

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+# --- Informations du Projet ---
+# Auteur : Armiya
+# Localisation : Paris, France
+# Sujet : Environnement de Biostatistique et Machine Learning
+
 print("Debut de l'aventure")
-prenom = "Armia"
-Pays = "France"
+prenom = "Armiya"
+pays = "France"
 ville = "Paris"
 
-print(f" Bonjour, mon Prenom : {prenom}, mon Pays : {Pays} et ma Ville: {ville}")
+# --- Imports des Packages ---
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+print(f"Utilisateur : {prenom} à {ville}, {pays}")
+print("Configuration terminée : Tous les modules sont importés avec succès !")
